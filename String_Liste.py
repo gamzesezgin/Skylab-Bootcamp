e213aba22431993a66cabcf02a3138982c56690b
Gamze Sezgin <gamzesezgin001@gmail.com>
2023-10-09 00:22:50 +0300

    String_Liste.py changed

--- String_Liste.py
+++ String_Liste.py
@@ -1,24 +1,57 @@
 # İçerisine girilen string ifadedeki kelimelerin hepsini bir listeye atan fonksiyon yazın.
 
-def list_maker(sentence):
-    new_word_list = sentence.split()
-    print(new_word_list)
+def kelimeleri_ayir(sentence):
+    kelimeler = []
+    kelime = ""
+    for karakter in sentence:
+        if karakter != " ":
+            kelime += karakter
+        else:
+            if kelime:
+                kelimeler.append(kelime)
+                kelime = ""
 
-list_maker(sentence= "Python ile stringi listeye dönüstürme")
+    if kelime:
+        kelimeler.append(kelime)
+
+    return kelimeler
+
+kelimeler = kelimeleri_ayir("SKY LABE HOSGELDİNİZ MERHABA")
+print(kelimeler)
 
 # İçerisine iki tane string değer alan ve bu string ifadelerin içerisinde ortak harfleri ve bu ortak harflerin kaçartane kullanıldığını belirleyen fonksiyon yazın.
 
-def common_char(str1, str2):
-    count = 0
-    for char in str1:
-        if char in str2:
-            counter1 = str1.count(char)
-            counter2 = str2.count(char)
-            print(char, counter1, counter2)
+def ortak_harfleri_bul(str1, str2):
+    str1 = str1.lower()
+    str2 = str2.lower()
 
-common_char("Anahtar", "araba")
-common_char("deniz", "beniz")
+    ortak_harfler = {}
 
+    for harf in str1:
+        if harf.isalpha() and harf in str2:
+            if harf in ortak_harfler:
+                ortak_harfler[harf][0] += 1
+            else:
+                ortak_harfler[harf] = [1,0]
+
+    for harf in str2:
+        if harf.isalpha() and harf in str1:
+            if harf in ortak_harfler:
+                ortak_harfler[harf][1] += 1
+            else:
+                ortak_harfler[harf] = [0,1]
+
+    for harf, (sayi1, sayi2) in ortak_harfler.items():
+        print(f"{harf} {sayi1} {sayi2}")
+
+string1 = "Anahtar"
+string2 = "araba"
+
+string3 = "deniz"
+string4 = "beniz"
+
+ortak_harfleri_bul(string1, string2)
+ortak_harfleri_bul(string3, string4)
 
 
 
